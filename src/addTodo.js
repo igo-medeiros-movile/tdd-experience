34962f61b5073d3a03e5f1646cb73ca98318d189
Willian Serafim <willian.serafim@sinch.com>
2022-04-20 10:55:52 -0300

    test: wip: refactor addTodoList test

---
+++ src/addTodo.js
@@ -0,0 +1,9 @@
+function addToDoList(todoList, item) { 
+    todoList.push(item);
+
+
+  return todoList;
+}
+
+export default addToDoList;
+ 
--- src/addTodo.test.js
+++ src/addTodo.test.js
@@ -1,5 +1,12 @@
-describe('add item to todo list', () => {
-    test('should add item to a list', () => {
-        expect(addToDoList('buy milk')).contains('buy milk');
-    })
-})
+import addToDoList from "./addTodo";
+
+describe("add item to todo list", () => {
+    test("should add item to a list", () => {
+        let toDoList = []; 
+        addToDoList(toDoList, "list item");
+        addToDoList(toDoList, "list item2");
+
+        expect(toDoList).toContain("list item");
+        expect(toDoList).toContain("list item2");
+    });
+});
